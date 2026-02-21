f31f4ebe294676f83da5982fb15b7bed28ae51b0
Anubhav <anubhavfordev24@gmail.com>
2026-02-22 01:37:45 +0530

    authApi.js updates

--- frontend/src/api/authApi.js
+++ frontend/src/api/authApi.js
@@ -1,4 +1,11 @@
 import API from "./axios"
 
-export const registerUser = (data) => API.post("/auth/register", data)
-export const loginUser = (data) => API.post("/auth/login", data)
+// REGISTER
+export const registerUser = (data) => {
+  return API.post("/auth/register", data)
+}
+
+// LOGIN
+export const loginUser = (data) => {
+  return API.post("/auth/login", data)
+}
